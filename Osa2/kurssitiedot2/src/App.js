29afaa0726fb6158629dceb0c339178cb35d698a
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-02-03 17:24:38 +0200

    Tehtävät 2.2-3

--- Osa2/kurssitiedot2/src/App.js
+++ Osa2/kurssitiedot2/src/App.js
@@ -3,16 +3,22 @@ const Header = ({ course }) =>
 
 const Part = ({ part }) => 
   <p>{part.name} {part.exercises}</p>
-
-
+  
 const Content = ({ parts }) =>{
   return (
     <div>
-      
-        {parts.map(part => 
-          <Part key={part.id} part={part} />
-        )}
-      
+      {parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <div>
+      <b>total of {parts.map(item => item.exercises).reduce((prev, cur) => prev + cur, 0
+      )} exercises</b>
     </div>
   )
 }
@@ -21,6 +27,7 @@ const Course = ({course}) =>
   <div>
     <Header course={course} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
 
 
