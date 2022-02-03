63fd273a1249317360b7fa4738a40f8f57dd4987
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-02-03 22:56:00 +0200

    Tehtävät 2.4

--- Osa2/kurssitiedot2/src/App.js
+++ Osa2/kurssitiedot2/src/App.js
@@ -1,5 +1,5 @@
 const Header = ({ course }) =>
-  <h1>{course.name}</h1>
+  <h3>{course.name}</h3>
 
 const Part = ({ part }) => 
   <p>{part.name} {part.exercises}</p>
@@ -30,33 +30,68 @@ const Course = ({course}) =>
     <Total parts={course.parts} />
   </div>
 
-
-function App() {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+const Multicourse = ({ courses }) =>{
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+
+  return (
+    <div>
+      <h2>Web development curriculum</h2>
+      <Multicourse courses={courses} />
     </div>
   )
 }
